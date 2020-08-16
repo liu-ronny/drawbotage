5e079dd760874ba47e40a78762c54c471971d9ea
liu-ronny <54465266+liu-ronny@users.noreply.github.com>
2020-08-15 20:35:52 -0700

    Deactivate bulldoze tool since it's not ready

--- src/api/drawbotage.js
+++ src/api/drawbotage.js
@@ -1,4 +1,25 @@
-const titles = ["reverse", "hide", "color", "bulldoze"];
+// const titles = ["reverse", "hide", "color", "bulldoze"];
+const titles = ["reverse", "hide", "color"];
+// const drawbotages = {
+//   reverse: {
+//     description:
+//       "Every stroke made by the player appears in the opposite direction.",
+//     iconClassName: "fa fa-map-signs",
+//   },
+//   hide: {
+//     description: "The player only sees the previous stroke on his/her canvas.",
+//     iconClassName: "far fa-eye-slash",
+//   },
+//   color: {
+//     description: "Each new stroke is set to a random color.",
+//     iconClassName: "fa fas fa-palette",
+//   },
+//   bulldoze: {
+//     description:
+//       "Every 10 seconds, 1/5th of the canvas gets bulldozed by a moving eraser.",
+//     iconClassName: "fa fas fa-eraser",
+//   },
+// };
 const drawbotages = {
   reverse: {
     description:
@@ -12,11 +33,6 @@ const drawbotages = {
   color: {
     description: "Each new stroke is set to a random color.",
     iconClassName: "fa fas fa-palette",
-  },
-  bulldoze: {
-    description:
-      "Every 10 seconds, 1/5th of the canvas gets bulldozed by a moving eraser.",
-    iconClassName: "fa fas fa-eraser",
   },
 };
 
